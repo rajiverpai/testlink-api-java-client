de785e7d1c321ff25d05799d97e906a426939249
brunodepaulak@gmail.com <brunodepaulak@gmail.com@6e0cc185-f2b2-5783-fda5-59ec1d66affd>
2010-10-21 18:04:22 +0000

    Junit tests for creation of Test Projects.

---
+++ branches/BUG-0001/testlink-api-java-client/src/testlink/api/java/client/junit/autoexec/TestProjectManagement.java
@@ -0,0 +1,78 @@
+/*
+ * Daniel R Padilla
+ *
+ * Copyright (c) 2009, Daniel R Padilla
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
+package testlink.api.java.client.junit.autoexec;
+
+import junit.framework.TestCase;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import testlink.api.java.client.TestLinkAPIClient;
+import testlink.api.java.client.TestLinkAPIException;
+
+/**
+ * @author Bruno P. Kinoshita - http://www.kinoshita.eti.br
+ * @since 21/10/2010
+ */
+public class TestProjectManagement 
+extends TestCase
+{
+
+	protected String devKey = "de4d6af9582c63cf624910bb4c861939";
+	protected String url = "http://localhost/testlink_cvs/lib/api/xmlrpc.php";
+	
+	private TestLinkAPIClient client;
+	
+	private String projectName = "testlink-api-java-client-test-project";
+	private String testCasePrefix = "tlj";
+	private String description = "Test Project for testlink-api-java-client";
+	
+	private Integer createdProjectId = 0;
+	
+	@Before
+	public void setUp()
+	{
+		client = new TestLinkAPIClient(devKey, url);
+	}
+	
+	@Test
+	public void testCreateSimpleProject()
+	{
+		try
+		{
+			createdProjectId = client.createTestProject(projectName, testCasePrefix, description, false, false, false, false, false, false);
+			assertTrue( createdProjectId > 0);
+		} catch (TestLinkAPIException e)
+		{
+			e.printStackTrace();
+			fail("Failed to ping: " + e.getMessage());
+		}
+	}
+	
+	@Test
+	public void testDeleteSimpleProject()
+	{
+		//assertTrue(createdProjectId > 0);
+		
+		
+	}
+	
+}
